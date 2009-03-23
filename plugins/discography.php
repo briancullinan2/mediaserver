3586e149a0b7227f7bed7d13280cb75f64979505
share <megamindbrian@gmail.com>
2009-03-23 02:16:35 +0000

    git-svn-id: svn://localhost/Media%20Server@149 cc2d05dc-c430-0410-aecc-8448c616cd3b

---
+++ plugins/discography.php
@@ -0,0 +1,5 @@
+<?php
+
+// download wikipedia pages for artists and provide links for the album information
+//  do amazon search, newzbin search, and google search for artist and albums
+//  list albums not in library
