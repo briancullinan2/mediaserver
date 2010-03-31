cfa9b331a91a5d00709fe64cb558c52d5e7d6c1f
bjcullinan <megamindbrian@gmail.com>
2010-03-31 19:09:39 +0000

    git-svn-id: svn://localhost/Media%20Server@282 cc2d05dc-c430-0410-aecc-8448c616cd3b

--- templates/plain/index.php
+++ templates/plain/index.php
@@ -11,12 +11,12 @@ function theme_plain_list()
         Get the list:
         <br />
         <form action="<?php print href('plugin=list'); ?>" method="get">
-            <input type="hidden" name="cat" value="{$cat}" />
+            <input type="hidden" name="cat" value="<?php print $GLOBALS['templates']['vars']['cat']; ?>" />
             Type <select name="list">
             	<?php
-				foreach($GLOBALS['lists'] as $type => $type)
+				foreach($GLOBALS['lists'] as $type => $list)
 				{
-					?><option value="<?php print $type['name']; ?>">{$type.name}</option><?php
+					?><option value="<?php print $type; ?>"><?php print $list['name']; ?></option><?php
 				}
 				?>
             </select>
