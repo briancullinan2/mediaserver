eef0fd8c1a0fa890c7fd7f5cc9ab8952bc7046a1
bjcullinan <megamindbrian@gmail.com>
2010-08-09 21:32:54 +0000

    git-svn-id: svn://localhost/Media%20Server@340 cc2d05dc-c430-0410-aecc-8448c616cd3b

--- handlers/discography.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// download wikipedia pages for artists and provide links for the album information
-//  do amazon search, newzbin search, and google search for artist and albums
-//  list albums not in library
-//  use parseFilename to search with
-
-?>
